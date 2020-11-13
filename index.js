1b9ad59dd6329ec407912027144b5a74afb98eed
sulenchy <sulaimanabiodun172@gmail.com>
2020-11-13 22:43:06 +0100

    set up node server

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 var http = require('http')
 var express = require('express')
-// var routes = require('./routes')
-// var user = require('./routes/user')
 var path = require('path')
 
-// var favicon = require('serve-favicon')
-// var logger = require('morgan')
-// var methodOverride = require('method-override')
-// var session = require('express-session')
-// var bodyParser = require('body-parser')
-// var multer = require('multer')
-// var errorHandler = require('errorhandler')
+var favicon = require('serve-favicon')
+var bodyParser = require('body-parser')
 
 var app = express()
 
 // all environments
 app.set('port', process.env.PORT || 3000)
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'pug')
 // app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
-// app.use(logger('dev'))
-// app.use(methodOverride())
-// app.use(session({
-//   resave: true,
-//   saveUninitialized: true,
-//   secret: 'uwotm8'
-// }))
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(multer())
 // app.use(express.static(path.join(__dirname, 'public')))
 
 // app.get('/', routes.index)
 // app.get('/users', user.list)
 
-// // error handling middleware should be loaded after the loading the routes
-// if (app.get('env') === 'development') {
-//   app.use(errorHandler())
-// }
+const { graphql } = require("@octokit/graphql");
+
+const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token 624ce67f41026d19f2134ba859253ec120932788`,
+    },
+  });
+  const { repository } = graphqlWithAuth(`
+    {
+        viewer {
+            login
+            starredRepositories {
+              totalCount
+            }
+            avatarUrl
+            repositories(last: 20) {
+              edges {
+                node {
+                  name
+                  stargazers {
+                    totalCount
+                  }
+                  watchers {
+                    totalCount
+                  }
+                  description
+                  createdAt
+                  languages(first: 10) {
+                    nodes {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+    }
+  `).then(res => {
+      console.log('res ===> ', res);
+      return res;
+  }).catch(err => err);
 
 var server = http.createServer(app)
 server.listen(app.get('port'), function () {
