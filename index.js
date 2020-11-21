1a246938535c0b89704cf53321d3fb870ddd0bc9
sulenchy <sulaimanabiodun172@gmail.com>
2020-11-21 11:08:23 +0100

    merge branches to master and install nodemon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http')
 var express = require('express')
 var path = require('path')
+var routers = require('./routes/index')
 
 var favicon = require('serve-favicon')
 var bodyParser = require('body-parser')
@@ -10,15 +11,10 @@ var app = express()
 // all environments
 app.set('port', process.env.PORT || 3000)
 // app.use(favicon(path.join(__dirname, '/public/favicon.ico')))
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(express.static(path.join(__dirname, 'public')))
 
-// app.get('/', routes.index)
-// app.get('/users', user.list)
-
+app.use('/ggql', routers)
 app.use('/client', express.static('public'));
 
 app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'))
+  console.log('Express server listening on port ' + app.get('port'));
 })
